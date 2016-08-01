fab143e592c0062994265f92532eb03b61044199
Natan Camargos <natan.camargos@ifactory.com.br>
2016-08-01 14:18:07 -0300

    getting started completed!

--- 04-intro-4/box.glsl
+++ 04-intro-4/box.glsl
@@ -1,8 +1,6 @@
 bool inBox(highp vec2 lo, highp vec2 hi, highp vec2 p) {
-
   //Test if the point p is inside the box bounded by [lo, hi]
-
-  return false;
+  return all(lessThanEqual(p, hi)) && all(greaterThanEqual(p, lo));
 }
 
 
